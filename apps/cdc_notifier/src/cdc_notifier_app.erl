616388be7165df1685de972639e13419916a40c1
ttt161 <losto@nix>
2026-04-23 11:50:26 +0300

    improve observability

--- apps/cdc_notifier/src/cdc_notifier_app.erl
+++ apps/cdc_notifier/src/cdc_notifier_app.erl
@@ -10,6 +10,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    _ = start_listener(),
     _ = application:ensure_all_started(cdc_progressor),
     cdc_notifier_sup:start_link().
 
@@ -17,3 +18,12 @@ stop(_State) ->
     ok.
 
 %% internal functions
+
+start_listener() ->
+    Port = application:get_env(cdc_notifier, port, 8022),
+    Routes = cowboy_router:compile([
+        {'_', [
+          {"/metrics/[:registry]", prometheus_cowboy2_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http_listener, [{port, Port}], #{env => #{dispatch => Routes}}).
